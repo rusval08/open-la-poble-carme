c3a1529653a1d1040760c803b4dd75214d21f2b8
rusval08 <vrppool.life@gmail.com>
2026-06-04 01:58:01 +0200

    Add logout functionality to admin.js

--- public/admin.js
+++ public/admin.js
@@ -1,1 +1,6 @@
+const logoutButton = document.querySelector("#logoutButton");
 
+logoutButton?.addEventListener("click", async () => {
+  await fetch("/api/logout", { method: "POST" });
+  window.location.href = "/login";
+});
