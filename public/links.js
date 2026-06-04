703ee323e8a050b86e000cbb05352f7c950a0b88
rusval08 <vrppool.life@gmail.com>
2026-06-04 02:02:01 +0200

    Add dynamic links and QR code generation

--- public/links.js
+++ public/links.js
@@ -1,1 +1,16 @@
+const origin = window.location.origin;
+const links = {
+  results: `${origin}/resultats`,
+  bracket: `${origin}/quadrant`,
+  youtube: "https://www.youtube.com/@carme8pool810/streams",
+};
 
+function qrUrl(value) {
+  return `https://api.qrserver.com/v1/create-qr-code/?size=260x260&data=${encodeURIComponent(value)}`;
+}
+
+document.querySelector("#resultsUrl").textContent = links.results;
+document.querySelector("#bracketUrl").textContent = links.bracket;
+document.querySelector("#resultsQr").src = qrUrl(links.results);
+document.querySelector("#bracketQr").src = qrUrl(links.bracket);
+document.querySelector("#youtubeQr").src = qrUrl(links.youtube);
